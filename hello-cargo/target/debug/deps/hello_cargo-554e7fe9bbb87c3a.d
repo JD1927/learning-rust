2c43e12be8245bc956031e6c1476f965f21b0684
Juan Aguirre <j1927aguirre@gmail.com>
2022-11-08 19:11:38 -0500

    Adjust gitignore & remove unnecessary files

--- hello-cargo/target/debug/deps/hello_cargo-554e7fe9bbb87c3a.d
+++
@@ -1,5 +0,0 @@
-/home/jd1927/jd-dev/repositories/learning-rust/hello-cargo/target/debug/deps/hello_cargo-554e7fe9bbb87c3a: src/main.rs
-
-/home/jd1927/jd-dev/repositories/learning-rust/hello-cargo/target/debug/deps/hello_cargo-554e7fe9bbb87c3a.d: src/main.rs
-
-src/main.rs:
--- hello-cargo/target/debug/hello-cargo.d
+++
@@ -1,1 +0,0 @@
-/home/jd1927/jd-dev/repositories/learning-rust/hello-cargo/target/debug/hello-cargo: /home/jd1927/jd-dev/repositories/learning-rust/hello-cargo/src/main.rs
